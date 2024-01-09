6bd82e8adb830957306d4fba80f7e53d4bfbfa73
JeniaGurman <144277083+JeniaGurman@users.noreply.github.com>
2024-01-09 12:12:00 +0200

    homework 7

--- js/task-6.js
+++ js/task-6.js
@@ -4,6 +4,8 @@ function getRandomHexColor() {
     .padStart(6, '0')}`;
 }
 
+
+const controls = document.getElementById('controls');
 const input = document.querySelector('input');
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
@@ -11,29 +13,29 @@ const boxesContainer = document.getElementById('boxes');
 
 createButton.addEventListener('click', () => {
   const amount = Number(input.value);
-
+  
   if (amount >= 1 && amount <= 100) {
     createBoxes(amount);
     input.value = '';
-  } else {
-    alert('Enter a number from 1 to 100');
   }
+  
 });
 
-destroyButton.addEventListener('click', () => {
-  boxesContainer.innerHTML = '';
-});
+destroyButton.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
-  let size = 30;
+  destroyBoxes();
 
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
+    box.classList.add('box');
     box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
     boxesContainer.appendChild(box);
-
-    size += 10;
   }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ''; 
 }
